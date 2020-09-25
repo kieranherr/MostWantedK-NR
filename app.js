39bd997e414b227352c406c5b46016d1723415d7
NCRadtke00 <raddnc@gmail.com>
2020-09-25 14:02:23 -0500

    created outline for function's that search each trait option

--- app.js
+++ app.js
@@ -66,7 +66,12 @@ function searchByTraits(people) {
     }
   }
 }
-
+function searchByGender(people) {}
+function searchByAge(people) {}
+function searchByHeight(people) {}
+function searchByWeight(people) {}
+function searchByEyeColor(people) {}
+function searchByOccupation(people) {}
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people) {
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
